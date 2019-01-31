3fae18a580874f79fd0cc9eb8dbde2e689b1c520
Lukasz Formela <39266504+LukaszFormela@users.noreply.github.com>
2019-01-31 13:16:31 +0000

    Improve cropping, add alias to image name

--- lt_image_resize.php
+++ lt_image_resize.php
@@ -25,17 +25,15 @@ function lt_image_resize ( $image_url, $width, $height ) {
 		return $image_url;
 	}
 	
-	
 	// If image has been resized already,
 	// return it's current path instead
 	$path_info = pathinfo( $image_path );
-	$resized_image_file_name = $path_info['filename'] . '-' . $width . 'x' . $height . '.' . $path_info['extension'];
+	$resized_image_file_name = $path_info['filename'] . '-lt-resized-' . $width . 'x' . $height . '.' . $path_info['extension'];
 	$resized_image_url = $image_base . $resized_image_file_name;
 	$resized_image_path = $path_info['dirname'] . '/' . $resized_image_file_name;
 	if ( is_file($resized_image_path) ) {
 		return $resized_image_url;
 	}
-	
 	
 	// If something's wrong with the editor, abort
 	$image = wp_get_image_editor($image_path);
@@ -43,7 +41,6 @@ function lt_image_resize ( $image_url, $width, $height ) {
 		return false;
 	}
 
-	
 	// Get the current image ratio.
 	// Because of how WordPress handles image cropping, 
 	// we'll resize base image to highest possible value,
@@ -53,9 +50,9 @@ function lt_image_resize ( $image_url, $width, $height ) {
 	$proposed_ratio = $width / $height;
 
 	if( $current_ratio > $proposed_ratio ) {
-		$image->resize($width, '9999');
-	} else {
 		$image->resize('9999', $height);
+	} else {
+		$image->resize($width, '9999');
 	}
 
 	$new_size = $image->get_size();
@@ -63,6 +60,8 @@ function lt_image_resize ( $image_url, $width, $height ) {
 	$start_from_x = $half_width - ($width / 2);
 	$half_height = $new_size['height'] / 2;
 	$start_from_y = $half_height - ($height / 2);
+
+	$current_ratio > $proposed_ratio ? $start_from_y = 0 : $start_from_x = 0;
 
 	$image->crop($start_from_x, $start_from_y, $width, $height);
 	$image->save($resized_image_path);
